3bbaec1cb0b248bf443cc24567967161697267be
luiz boldrini <luizboldriniej@gmail.com>
2022-04-26 16:21:00 -0300

    Manipulando data da tabela

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -6,8 +6,7 @@ module.exports = app => {
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        res.send('Post antendimento')
+        Atendimento.adiciona(atendimento, res)
 
     });
 
