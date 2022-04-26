f94e9fd3fcced433003db7e30b328356384de944
luiz boldrini <luizboldriniej@gmail.com>
2022-04-26 15:46:05 -0300

    Consertando bud com MySQL

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,12 +1,15 @@
-const Atendimento = require('../models/atendimentos')
+const Atendimento = require('../models/antendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'));
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
         Atendimento.adiciona(atendimento)
-        res.send('Post atendimento')
-    })
+        res.send('Post antendimento')
+
+    });
+
+
 }
